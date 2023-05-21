95f2eda5bfe367826c11332d2e5169c28ac5b5f2
unknown <jenunezb@uqvirtual.edu.co>
2023-05-21 18:16:11 -0500

    31. Modifique la función que se llama en el evento de login en el archivo login.component.ts para que quede así: Funcionando

--- src/app/pagina/login/login.component.ts
+++ src/app/pagina/login/login.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { Alerta } from 'src/app/modelo/alerta';
 import { UsuarioDTO } from 'src/app/modelo/usuario-dto';
 import { AuthService } from 'src/app/servicios/auth.service';
+import { TokenService } from 'src/app/servicios/token.service';
 
 @Component({
   selector: 'app-login',
@@ -13,20 +14,19 @@ export class LoginComponent {
   alerta!: Alerta;
   usuario: UsuarioDTO;
 
-  constructor(private authService: AuthService) {
+  constructor(private authService: AuthService, private tokenService: TokenService) {
     this.usuario = new UsuarioDTO();
   }
 
-  iniciarSesion(): void {
+  public login(){
     const objeto = this;
     this.authService.login(this.usuario).subscribe({
-      next: data => {
-        objeto.alerta = new Alerta(data.respuesta, "success");
-      },
-      error: error => {
-        objeto.alerta = new Alerta(error.error.respuesta, "danger");
-      }
+    next: data => {
+    objeto.tokenService.login(data.respuesta.token);
+    },
+    error: error => {
+    objeto.alerta = new Alerta(error.error.respuesta, "danger");
+    }
     });
-    console.log(`Iniciando sesión con usuario: ${this.usuario} y contraseña: ${this.usuario.password}`);
-  }
+  }    
 }
