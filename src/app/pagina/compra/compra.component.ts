b2599d938b5920e43d9b5df4330ec98d902bbeff
unknown <jenunezb@uqvirtual.edu.co>
2023-05-28 16:24:38 -0500

    detalles de la compra definiendo la info del cliente y el producto, terminados

--- src/app/pagina/compra/compra.component.ts
+++ src/app/pagina/compra/compra.component.ts
@@ -12,45 +12,56 @@ import { UsuarioService } from 'src/app/servicios/usuario.service';
   templateUrl: './compra.component.html',
   styleUrls: ['./compra.component.css']
 })
-export class CompraComponent implements OnInit{
-  numero:number=0;
-  alerta!:Alerta;
-  codigo:any;
-  producto: any={};
-  route:any;
+export class CompraComponent implements OnInit {
+  numero: number = 0;
+  alerta!: Alerta;
+  codigo: any;
+  producto: any = {};
+  route: any;
   productos: ProductoGetDTO[];
+  usuario: any;
+  email:String="";
 
-  constructor(private productoServicio: ProductoService,private router: Router, private sharedData: SharedService, 
-    private token: TokenService, private usuarioServicio: UsuarioService){
-    this.producto=this.producto;
+  constructor(private productoServicio: ProductoService, private router: Router, private sharedData: SharedService,
+    private token: TokenService, private usuarioServicio: UsuarioService) {
+    this.producto = this.producto;
     this.productos = [];
+    this.email=token.getEmail();
   }
   ngOnInit(): void {
-    const url: string = window.location.href; 
+    const url: string = window.location.href;
     const segments: string[] = url.split('/');
-    
+
     const index: number = segments.indexOf('compra');
     if (index !== -1 && segments.length > index + 1) {
-      this.numero = parseInt(segments[index + 1]); 
+      this.numero = parseInt(segments[index + 1]);
     } else {
       console.log('No se encontró el número en la URL.');
     }
-     this.obtenerProducto();
-      }
+    this.obtenerProducto();
+    this.obtenerCliente();
+  }
 
-      public obtenerProducto(){
-        this.productoServicio.obtenerProducto(this.numero).subscribe({
-          next: data => {
-            this.alerta = new Alerta(data.respuesta, "success");
-            this.producto=data.respuesta;
-            },
-            error: error => {
-              this.alerta = new Alerta(error.error.respuesta, "danger");
-            }
-        });
+  public obtenerProducto() {
+    this.productoServicio.obtenerProducto(this.numero).subscribe({
+      next: data => {
+        this.alerta = new Alerta(data.respuesta, "success");
+        this.producto = data.respuesta;
+      },
+      error: error => {
+        this.alerta = new Alerta(error.error.respuesta, "danger");
       }
+    });
+  }
 
-      public obtenerCliente(){
-          this.usuarioServicio.cedula
-      }
-    }
+  public obtenerCliente() {
+  this.usuarioServicio.cedula(this.email).subscribe((valor: any) => {
+    this.codigo = valor.respuesta;
+this.usuarioServicio.obtener(this.codigo).subscribe((valor:any)=>{
+    this.usuario = valor.respuesta;
+    console.log(this.usuario)
+});
+  });
+  }
+
+}
