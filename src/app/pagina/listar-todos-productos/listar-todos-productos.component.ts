838815e0d14bbabd146bb3cfaa564b47b4dcf2cc
unknown <jenunezb@uqvirtual.edu.co>
2023-05-28 23:47:25 -0500

    agregar favoritos listo y con conexion entre back y front

--- src/app/pagina/listar-todos-productos/listar-todos-productos.component.ts
+++ src/app/pagina/listar-todos-productos/listar-todos-productos.component.ts
@@ -1,8 +1,12 @@
 import { Component, OnInit } from '@angular/core';
+import { Route, Router } from '@angular/router';
 import { ProductoGetDTO } from 'src/app/modelo/ProductoGetDTO';
 import { Alerta } from 'src/app/modelo/alerta';
+import { FavoritosService } from 'src/app/servicios/favoritos.service';
 import { ProductoService } from 'src/app/servicios/producto.service';
 import { SharedService } from 'src/app/servicios/shared.service';
+import { TokenService } from 'src/app/servicios/token.service';
+import { UsuarioService } from 'src/app/servicios/usuario.service';
 
 
 @Component({
@@ -14,44 +18,64 @@ export class ListarTodosProductosComponent implements OnInit {
   productos: ProductoGetDTO[];
   objeto: any;
   showMessage: boolean = false;
-  constructor(private productoServicio: ProductoService,  
-    private sharedService:SharedService){
+  codigoUsuario: number = 0;
+  email: String = "";
+  usuario: any;
+
+  constructor(private productoServicio: ProductoService,
+    private sharedService: SharedService, private token: TokenService,
+    private usuarioServicio: UsuarioService, private favoritoServicio: FavoritosService,
+    private route: Router) {
+    this.email = token.getEmail();
     this.productos = [];
     this.objeto = this;
   }
 
   ngOnInit(): void {
     this.getProductos();
+    this.obtenerCliente();
   }
 
-  public getProductos(){
-      
+  public getProductos() {
+
     this.productoServicio.getProductos().subscribe({
       next: data => {
         this.objeto.alerta = new Alerta(data.respuesta, "success");
         this.sharedService.updateObjeto(this.objeto);
         this.productos = data.respuesta;
-        },
-        error: error => {
-          this.objeto.alerta = new Alerta(error.error.respuesta, "danger");
-        }
+      },
+      error: error => {
+        this.objeto.alerta = new Alerta(error.error.respuesta, "danger");
+      }
     });
   }
 
-  guardarCodigoProducto(codigo: number){
+  guardarCodigoProducto(codigo: number) {
     this.sharedService.codigoProducto = codigo;
     console.log(codigo);
   }
 
-  addToFavorites(item: any) {
-    // Agrega lógica para guardar el producto en favoritos
-    // Puedes usar localStorage, un servicio, o cualquier otra forma de almacenamiento
-    // Ejemplo de almacenamiento en localStorage:
-    localStorage.setItem('favoritos', JSON.stringify(item));
-
-    this.showMessage = true;
-    setTimeout(() => {
-      this.showMessage = false;
-    }, 3000); // Oculta el mensaje después de 3 segundos
-  }
+  agregarFavoritos(item: any) {
+if(this.email!==""){
+  this.favoritoServicio.agregarFavoritos(item, this.codigoUsuario)
+  localStorage.setItem('favoritos', JSON.stringify(item));
+  this.showMessage = true;
+ setTimeout(() => {
+    this.showMessage = false;
+  }, 3000); // Oculta el mensaje después de 3 segundos
+}else{
+      this.route.navigate(["/login"]);
+}
+}
+  public obtenerCliente() {
+    if(this.email!==""){
+      console.log(this.email);
+      this.usuarioServicio.cedula(this.email).subscribe((valor: any) => {
+        this.codigoUsuario = valor.respuesta;
+       this.usuarioServicio.obtener(this.codigoUsuario).subscribe((valor: any) => {
+          this.usuario = valor.respuesta;
+        });
+      });
+   }
+    }
 }
---
+++ src/app/servicios/favoritos.service.ts
@@ -0,0 +1,20 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { MensajeDTO } from '../modelo/mensaje-dto';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class FavoritosService {
+  private catURL = "http://localhost:8080/api/favoritos";
+  constructor(private http: HttpClient) { }
+    public agregarFavoritos(codigoProducto: number, codigoUsuario:number):Observable<MensajeDTO>{
+      const favoritos = {
+        codigoProducto: codigoProducto,
+        codigoUsuario: codigoUsuario
+      };
+      return this.http.post<MensajeDTO>(`${this.catURL}/crear`,favoritos);
+    }
+  }
+
