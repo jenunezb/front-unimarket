014a295bf96337e5dbd4d2261baec85abb62800f
jenunezb <jenunezb@gmail.com>
2023-05-29 12:04:26 -0500

    Función de compra

--- src/app/pagina/miscompras/miscompras.component.ts
+++ src/app/pagina/miscompras/miscompras.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { UsuarioService } from 'src/app/servicios/usuario.service';
+import { TokenService } from 'src/app/servicios/token.service';
+import { CompraService } from 'src/app/servicios/compra.service';
+
 
 @Component({
   selector: 'app-miscompras',
@@ -7,4 +11,44 @@ import { Component } from '@angular/core';
 })
 export class MiscomprasComponent {
 
+  usuario:any;
+  email:any;
+
+  constructor(private token: TokenService, private usuarioService: UsuarioService, private compraService: CompraService){
+    this.email=this.token.getEmail();
+  }
+
+ngOnInit(): void {
+  this.obtenerCliente();
+
+}
+public obtenerCliente() {
+  if (this.email !== "") {
+    console.log(this.email);
+    this.usuarioService.cedula(this.email).subscribe((valor: any) => {
+      this.usuario = valor.respuesta;
+      this.getProductos();
+      this.usuarioService.obtener(this.usuario).subscribe((valor: any) => {
+        this.usuario = valor.respuesta;
+      });
+    });
+  }
+}
+
+public getProductos(){
+
+  if( this.token.getEmail()!=""){
+
+    this.compraService.obtenerlistaCompra(this.usuario).subscribe({
+      next: data => {
+
+       console.log(this.usuario, "usuario");
+       console.log(data.respuesta);
+
+        }
+    });
+  }else{
+    //this.route.navigate(["/"]);
+  }
+}
 }
